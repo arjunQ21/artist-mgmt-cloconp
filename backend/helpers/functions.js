58a20909fbd3aca29a5bc4e0afa65ee4cd537ce1
arjunQ21 <arjunq21@gmail.com>
2024-12-11 19:55:53 +0545

    Added and tested function to get user from a JWT Token

--- backend/helpers/functions.js
+++ backend/helpers/functions.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { getUser } from "../services/db/user.js";
 
 
 async function hashPassword (plainPassword) {
@@ -14,9 +15,22 @@ async function hashPassword (plainPassword) {
 }
 
 function createAuthTokenFor (userId) {
-  return jwt.sign({data: {id: userId}}, process.env.JWT_SECRET, {expiresIn: '100d'})
+  return jwt.sign({ data: { id: userId } }, process.env.JWT_SECRET, { expiresIn: '100d' })
 }
 
+async function getUserFromJWT (token) {
+  try {
+    var decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded)
+    const { id } = decoded.data;
+    return await getUser(id);
+  } catch (e) {
+    console.log("Error getting user from JWT: ", e);
+    return null;
+  }
+}
 
+// getUserFromJWT("eyJhbGciOiUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxfSwiaWF0IjoxNzMzOTI1NzYzLCJleHAiOjE3NDI1NjU3NjN9.X_GugQhhI-T_hD0ggIjLEs9UO_8YlbvtjXvzCYq7IJM")
+// .then(u => console.log(u))
 
-export { hashPassword, createAuthTokenFor }
+export { hashPassword, createAuthTokenFor, getUserFromJWT }

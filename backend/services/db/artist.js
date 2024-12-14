c8b7d2009ad15717fc50fe5921146ed193f54a0c
arjunQ21 <arjunq21@gmail.com>
2024-12-14 16:52:12 +0545

    Made artist database interface code

--- backend/services/db/artist.js
+++ backend/services/db/artist.js
@@ -3,9 +3,9 @@ import connection from "./connection.js";
 async function getArtist (id) {
     try {
         const [artistData] = await connection.query("SELECT * from artist WHERE id = ?", [id])
-        if (  artistData[0]) {
+        if (artistData[0]) {
             return { ...artistData[0], password: undefined };
-        } else throw new Error("Artist not found by id: "+ id)
+        } else throw new Error("Artist not found by id: " + id)
     } catch (err) {
         console.log("Error getting artist: ", err);
         throw err;
@@ -20,7 +20,7 @@ async function readArtists (page = 1, limit = 10) {
             'SELECT * FROM artist ORDER BY id DESC LIMIT ? OFFSET ?',
             [limit, offset]
         );
-        return rows.map(r => ({...r, password: undefined}));
+        return rows.map(r => ({ ...r, password: undefined }));
     } catch (err) {
         console.error('Error reading artists:', err.message);
         throw err;
@@ -29,23 +29,20 @@ async function readArtists (page = 1, limit = 10) {
 
 async function insertArtist (artist) {
     const {
-        first_name,
-        last_name,
-        role,
-        email,
-        password,
-        phone,
+        name,
         dob,
         gender,
         address,
+        first_release_year,
+        no_of_albums_released,
     } = artist;
 
     try {
         const [result] = await connection.query(
             `INSERT INTO artist 
-        (first_name, last_name, role,  email, password, phone, dob, gender, address) 
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-            [first_name, last_name, role, email, password, phone, dob, gender, address]
+        (name,dob,gender,address,first_release_year,no_of_albums_released) 
+        VALUES (?, ?, ?, ?, ?, ?)`,
+            [name, dob, gender, address, first_release_year, no_of_albums_released,]
         );
         const insertedId = result.insertId;
         if (!insertedId) {
@@ -76,16 +73,17 @@ async function updateArtist (id, updatedFields) {
     }
 }
 
-
-async function loginArtist ({ email, password }) {
-    const [results] = await connection.query("SELECT id, password FROM artist WHERE email = ?", [email]);
-    if (results[0]) {
-        if (!await bcrypt.compare(password, results[0].password)) {
-            throw new Error("Incorrect Password.");
-        } else return await getArtist(results[0].id);
-
-    } else throw new Error("Artist not found by email: " + email);
+async function deleteArtist (id) {
+    try {
+        const [artistData] = await connection.query("DELETE from artist WHERE id = ?", [id])
+        // console.log({ artistData })
+        if (artistData.affectedRows) {
+            return true;
+        } else throw new Error("Artist not found by id: " + id)
+    } catch (err) {
+        console.log("Error deleting artist: ", err);
+        throw err;
+    }
 }
 
-
-export { readArtists, insertArtist, updateArtist, getArtist, loginArtist }
+export { readArtists, insertArtist, updateArtist, getArtist, deleteArtist }
